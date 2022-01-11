59f4ccf739126cb50e44b03c6c245863d0406ab0
Sumit Chahar <chahar.sumit888@gmail.com>
2022-01-12 00:39:33 +0530

    Complete routes for community forum api

--- block-BNaafv/CommunityForum/models/Answer.js
+++ block-BNaafv/CommunityForum/models/Answer.js
@@ -7,6 +7,14 @@ const answerSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User" },
 }, { timestamps: true });
 
+answerSchema.methods.answerJSON = function(questionId){
+  return {
+    _id: this.id,
+    text: this.text,
+    questionId: questionId
+  }
+};
+
 const Answer = mongoose.model('Answer', answerSchema);
 
 module.exports = Answer;
--- block-BNaafv/CommunityForum/routes/answers.js
+++ block-BNaafv/CommunityForum/routes/answers.js
@@ -5,17 +5,52 @@ const Question = require('../models/Question');
 const Answer = require('../models/Answer');
 const slugger = require('slug');
 const randomString = require('randomstring');
-const { route } = require('.');
 
 const router = express.Router();
 
-// create Answer
+// update answers
 
-router.post('/', auth.verifyToken, async (req, res, next) => {
-  
+router.put('/:answerId', auth.verifyToken, async(req, res, next) => {
+  let id = req.params.answerId;
+  try{
+    let answer = await Answer.findById(id);
+    if(!answer){
+      return res.status(400).json({ error: "No Answer Found!!" });
+    }else if(req.user.userId === answer.author.toString()){
+      let updatedAnswer = await Answer.findByIdAndUpdate(id, req.body, {new: true}).populate('author', "_id username");
+      res.status(201).json({ answer: updatedAnswer });
+    }else{
+      res.status(400).json({ error: "You are not the owner of this answer" });
+    }
+  }catch(error){
+    next(error);
+  }
+});
+
+// update answers
+
+
+// delete answers
+
+router.delete('/:answersId', auth.verifyToken, async (req, res, next) => {
+  let id = req.params.answersId;
+  try{
+    let answer = await Answer.findById(id);
+    if(!answer){
+      return res.status(400).json({ error: "No Answer Found!!" });
+    }else if(req.user.userId === answer.author.toString()){
+      let updatedAnswer = await Answer.findByIdAndDelete(id).populate('author', "_id username");
+      let updatedQuestion = await Question.findOneAndUpdate({ answers: {$in: updatedAnswer.id} }, {$pull: {answers: updatedAnswer.id}}, {new: true});
+      console.log(updatedQuestion);
+      res.status(201).json({ answer: updatedAnswer });
+    }else{
+      res.status(400).json({ error: "You are not the owner of this answer" });
+    }
+  }catch(error){
+    next(error);
+  }
 })
 
-// create Answer
-
+// delete answers
 
 module.exports = router;
